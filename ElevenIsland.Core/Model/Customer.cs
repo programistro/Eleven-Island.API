c1b37b9261c6756df3215f0d9315235808fab967
prrogramistro <r.grigorev1502@gmail.com>
2024-11-22 20:52:43 +0300

    core

---
+++ ElevenIsland.Core/Model/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElevenIsland.Core;
+
+public class Customer
+{
+    [Key]
+    public Guid Id { get; set; }
+    
+    public string FirstName { get; set; }
+    
+    public string LastName { get; set; }
+    
+    public string Email { get; set; }
+    
+    public string PhoneNumber { get; set; }
+    
+    public string PasswordHash { get; set; }
+}
